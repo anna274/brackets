00b76933492bf6654697c093663cc4af6fcd4da7
anna274 <anna_rusakovich.01@mail.ru>
2020-03-13 21:22:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  bracketsConfig = bracketsConfig.map(item => item.join('')).join('  ');
+  let bracketsChecker = [];
+
+  for(let i = 0; i < str.length; i++){
+    let indexInConfig = bracketsConfig.indexOf(str[i]);
+
+    if(indexInConfig % 2 == 0){
+
+      if(bracketsConfig[indexInConfig] == bracketsConfig[indexInConfig + 1] && bracketsChecker[bracketsChecker.length - 1] == str[i]){
+        bracketsChecker.pop();
+        continue;
+      }else{
+        bracketsChecker.push(str[i]);
+      }
+      continue;
+    }
+
+    if(bracketsChecker[bracketsChecker.length - 1] == bracketsConfig[indexInConfig - 1]){
+      bracketsChecker.pop();
+    }else{
+      return false;
+    }  
+  }
+
+  if(bracketsChecker.length != 0){
+    return false;
+  }
+  return true;
 }
